98772435a27d7c61688c22de409fa0b0ae1da145
yKD1028 <kodai.developer@gmail.com>
2022-05-15 16:06:41 +0900

    ♻️ maneをコンポーネント化

--- components/Headline.jsx
+++ components/Headline.jsx
@@ -10,9 +10,9 @@ export function Headline(props) {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing
-          <code className={styles.code}>pages/{props.page}.js</code>
+          Get started by editing {props.children}
         </p>
+        {/* <button onClick={props.onClick}>BUTTON</button> */}
     </div>
 
   );
---
+++ components/Main.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+import {Links} from '../components/links'
+import {Headline} from '../components/Headline'
+
+export function Main(props) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Headline 
+          page={props.page}
+        >
+          <code className={styles.code}>pages/{props.page}.js</code>
+        </Headline>
+        <Links />
+      </main>
+    </div>
+  );
+}
